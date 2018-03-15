0c93ce32bceb425c31e47b556857eea2ee0c51a4
adri <adri.skrtic@gmail.com>
2018-03-15 13:51:57 -0500

    changing connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const connectionString = 'mongodb://adriskrtic:adriskrtic@ds215019.mlab.com:15019/todo1';
+const connectionString = process.env.CONNECTION_STRING; //'mongodb://adriskrtic:adriskrtic@ds215019.mlab.com:15019/todo1';
 //connectionString = 'mongodb://localhost:27017/TodoApp'
 mongoose.connect(connectionString);
 
