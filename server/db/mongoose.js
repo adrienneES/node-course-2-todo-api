458daf77d08dd8ff3e51026007b9de86c50aac94
adri <adri.skrtic@gmail.com>
2018-03-14 19:44:00 -0500

    fixed mongoose entry for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const connectionString = 'mongodb://<dbuser>:<dbpassword>@ds215019.mlab.com:15019/todo1';
+const connectionString = 'mongodb://adriskrtic:adriskrtic@ds215019.mlab.com:15019/todo1';
 //connectionString = 'mongodb://localhost:27017/TodoApp'
 mongoose.connect(connectionString);
 
