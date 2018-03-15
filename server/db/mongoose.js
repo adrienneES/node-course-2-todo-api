993caffce82dc29d8d40e61035f14263c1171100
adri <adri.skrtic@gmail.com>
2018-03-14 20:16:07 -0500

    fixing connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-const connectionString = 'mongodb://adriskrtic:adriskrtic@ds215019.mlab.com:15019/todo1';
+
+const port = process.env.CONNECTION_STRING || 'mongodb://localhost:27017/TodoApp';
+
+//const connectionString = 'mongodb://adriskrtic:adriskrtic@ds215019.mlab.com:15019/todo1';
 //connectionString = 'mongodb://localhost:27017/TodoApp'
 mongoose.connect(connectionString);
 
